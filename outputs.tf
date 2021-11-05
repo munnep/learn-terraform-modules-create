4be1efe5f5b6206614de1dc8529033dd7003cb46
munnep <patrick.munne@hashicorp.com>
2021-11-05 09:46:07 +0100

    first try

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,5 @@
 # Output variable definitions
 
-output "vpc_public_subnets" {
-  description = "IDs of the VPC's public subnets"
-  value       = module.vpc.public_subnets
-}
-
-output "ec2_instance_public_ips" {
-  description = "Public IP addresses of EC2 instances"
-  value       = module.ec2_instances.public_ip
-}
-
 output "website_bucket_arn" {
   description = "ARN of the bucket"
   value       = module.website_s3_bucket.arn
@@ -23,4 +13,9 @@ output "website_bucket_name" {
 output "website_bucket_domain" {
   description = "Domain name of the bucket"
   value       = module.website_s3_bucket.domain
+}
+
+output "website_endpoint" {
+  description = "Domain name of the bucket"
+  value       = module.website_s3_bucket.website
 }
